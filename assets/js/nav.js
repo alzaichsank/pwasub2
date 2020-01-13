22defe6ae9fe1bd785321bfc62ef5a17d7ee59f8
Alza Devloper <alzaichsank@gmail.com>
2020-01-14 02:13:22 +0700

    finish teamsdetails

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -377,13 +377,91 @@ const route = (page) => {
                                 var table_row = ''
 
                                 data.squad.reverse().forEach(item => {
-                                    table_row += `
-                                    <tr>
-                                    <td>${item.name}</td>
-                                    <td>${item.position}</td>
-                                    <td>${item.nationality}</td>
-                                    </tr>
-                                    `
+                                    if(item.position == null ){
+                                        table_row += `
+                                            <tr>
+                                            <td>${item.name}</td>
+                                            <td>-</td>
+                                            <td>${item.nationality}</td>
+                                            <td class="text-center">
+                                            <div class="card-action">
+                                                <a
+                                                    data-team="${data.name}"
+                                                    data-flag="${data.crestUrl.replace(/^http:\/\//i, 'https://')}"
+                                                    data-name="${item.name}"     
+                                                    data-position="-" 
+                                                    data-nationality="${item.nationality}" 
+                                                    data-birth="${item.dateOfBirth.substr(0,10)}"
+                                                    class="modal-trigger"  href="#playerdata">Detail
+                                                </a>
+                                            </div>
+                                            </td>
+                                            </tr>
+                                            `
+                                    }else if(item.name == null ){
+                                        table_row += `
+                                        <tr>
+                                        <td>-</td>
+                                        <td>${item.position}</td>
+                                        <td>${item.nationality}</td>
+                                        <td class="text-center">
+                                            <div class="card-action">
+                                                <a
+                                                    data-team="-"
+                                                    data-flag="${data.crestUrl.replace(/^http:\/\//i, 'https://')}"
+                                                    data-name="${item.name}"     
+                                                    data-position="${item.position}" 
+                                                    data-nationality="${item.nationality}" 
+                                                    data-birth="${item.dateOfBirth.substr(0,10)}"
+                                                    class="modal-trigger"  href="#playerdata">Detail
+                                                </a>
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        `
+                                    }else if(item.nationality == null ){
+                                        table_row += `
+                                        <tr>
+                                        <td>${item.name}</td>
+                                        <td>${item.position}</td>
+                                        <td>-</td>
+                                        <td class="text-center">
+                                            <div class="card-action">
+                                                <a
+                                                    data-team="${data.name}"
+                                                    data-flag="${data.crestUrl.replace(/^http:\/\//i, 'https://')}"
+                                                    data-name="${item.name}"     
+                                                    data-position="${item.position}" 
+                                                    data-nationality="-" 
+                                                    data-birth="${item.dateOfBirth.substr(0,10)}"
+                                                    class="modal-trigger"  href="#playerdata">Detail
+                                                </a>
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        `
+                                    }else{
+                                        table_row += `
+                                        <tr>
+                                        <td>${item.name}</td>
+                                        <td>${item.position}</td>
+                                        <td>${item.nationality}</td>
+                                        <td class="text-center">
+                                            <div class="card-action">
+                                                <a
+                                                    data-team="${data.name}"
+                                                    data-flag="${data.crestUrl.replace(/^http:\/\//i, 'https://')}"
+                                                    data-name="${item.name}"     
+                                                    data-position="${item.position}" 
+                                                    data-nationality="${item.nationality}" 
+                                                    data-birth="${item.dateOfBirth.substr(0,10)}"
+                                                    class="modal-trigger"  href="#playerdata">Detail
+                                                </a>
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        `
+                                    }
                                 })
                                 var card = `
                                 <div class="row">
@@ -400,20 +478,20 @@ const route = (page) => {
                                 <div class="card-content">
                                 <table class="info-club">
                                 <tr>
-                                <td>Nationality</td>
+                                <td>Negara</td>
                                 <td>${data.area.name}</td>
                                 </tr>
                                 <tr>
-                                <td>Founded</td>
+                                <td>Berdiri</td>
                                 <td>${data.founded}</td>
                                 </tr>
                                 <tr>
-                                <td>Address</td>
+                                <td>Alamat</td>
                                 <td>${data.address}</td>
                                 </tr>
                                 <tr>
-                                <td>Club Color</td>
-                                <td>${data.clubColors}</td>
+                                <td>Markas</td>
+                                <td>${data.venue}</td>
                                 </tr>
                                 <tr>
                                 <td>Website</td>
@@ -425,22 +503,21 @@ const route = (page) => {
                                 </tr>
                                 </table>
                                 </div>
-                                <div class="card-content team-squad hide" style="display: none">
+                                <div class="card-content team-squad">
+                                <span class="team-icon-title card-title" style="color:white;">Data Pemain</span>
                                 <table class="striped">
                                 <thead>
                                 <tr>
-                                <td>Name</td>
-                                <td>Position</td>
-                                <td>Nationaility</td>
+                                <td class="text-center">Nama</td>
+                                <td class="text-center">Posisi</td>
+                                <td class="text-center">Negara</td>
+                                <td class="text-center">Aksi</td>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 ${table_row}
                                 </tbody>
                                 </table>
-                                </div>
-                                <div class="card-action">
-                                <a href="#" id="show-squad">show all squad</a>
                                 </div>
                                 </div>
                                 </div>
@@ -452,22 +529,60 @@ const route = (page) => {
                                 <a class="btn-floating btn-large waves-effect waves-light blue darken-1 to-top"><i class="material-icons">expand_less</i></a>
                                 </div>
                                 `
+                                card += `
+                                <div id="playerdata" class="modal modal-round">
+                                </div>`
+
                                 root.html(card)
 
-                                $('#show-squad').click((e) => {
-                                    e.preventDefault()
-                                    var elem = $('.team-squad')
-                                    var btn = $('#show-squad')
-
-                                    if (elem.hasClass('hide')) {
-                                        elem.show('slow')
-                                        elem.removeClass('hide')
-                                        btn.text('less all squad')
-                                    } else {
-                                        elem.hide('slow')
-                                        elem.addClass('hide')
-                                        btn.text('show all squad')
-                                    }
+                                $('.modal-trigger').each(function () {
+                                    var $this = $(this);
+                                    $this.on("click", function () {
+                                        console.log("click playerdata detail")
+                                        // alert($(this).data('name'));
+                                         var info = `
+                                         <div class="row">
+                                            <div class="col s12 m12">
+                                            <div class="card">
+                                            <div>
+                                                <div class="card-image height">
+                                                    <img class="icon-table" src="${data.crestUrl.replace(/^http:\/\//i, 'https://')}">
+                                                </div>
+                                                <div class="card-image">
+                                                    <span class="team-icon-title card-title">${data.name}</span>
+                                                </div>
+                                            </div>
+                                            <table class="info-club">
+                                                <tr>
+                                                    <td>Nama</td>
+                                                    <td>${$(this).data('name')}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Posisi</td>
+                                                    <td>${$(this).data('position')}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Negara</td>
+                                                    <td>${$(this).data('nationality')}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Tim</td>
+                                                    <td>${$(this).data('team')}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Tanggal Lahir</td>
+                                                    <td>${$(this).data('birth')}</td>
+                                                </tr>
+                                             </table>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        </div>`
+                                        $('#playerdata').html(info);
+                                         $('#playerdata').modal()
+                                         console.log("click modal detail")
+                                    });
+                                    
                                 })
                                 $('.btn-back').click(() => {
                                     window.location.hash = '#favorite';
